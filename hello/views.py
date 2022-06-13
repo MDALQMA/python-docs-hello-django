4c91f7ba3483cbc3f10cd25f4209b000c624ea9e
MDALQMA <63184535+MDALQMA@users.noreply.github.com>
2022-06-13 10:53:28 +0530

    Update views.py
    Updated just output for console.

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello team , happy weekend")
+    return HttpResponse("Hello team , We are learning Azure and having a great time.")
